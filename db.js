59f161f78e9733122eaa2a9cf02489fc08921f27
migueljallaza <38589552+migueljallaza@users.noreply.github.com>
2023-10-28 22:40:47 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 // The secret connection string you copied earlier
 const connectionString =
-  "postgresql://postgres:0VwOB8LCFrMGG7GkGVXk@containers-us-west-161.railway.app:7492/railway";
+  "postgres://postgres:5GCg6Ec5bGf3**bb6dG5DafCF5cG-CG6@postgres.railway.internal:5432/railway";
 
 const pool = new Pool({
   connectionString,
